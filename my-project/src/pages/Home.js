829a9fb266fc58f069fe4c791c6d2b63ba4a014e
sofiaaa05 <sofianugraheni05@gmail.com>
2025-10-29 15:40:12 +0700

    update kode baru home.js

---
+++ my-project/src/pages/Home.js
@@ -0,0 +1,25 @@
+
+import React from 'react';
+import Navbar from '../components/Navbar';
+import ProfileCard from '../components/ProfileCard';
+import AboutSection from '../components/AboutSection';
+import SkillsSection from '../components/SkillsSection';
+import PortfolioSection from '../components/PortfolioSection';
+import ContactSection from '../components/ContactSection';
+import Footer from '../components/Footer';
+
+function Home() {
+  return (
+    <div className="min-h-screen">
+      <Navbar />
+      <ProfileCard />
+      <AboutSection />
+      <SkillsSection />
+      <PortfolioSection />
+      <ContactSection />
+      <Footer />
+    </div>
+  );
+}
+
+export default Home;
